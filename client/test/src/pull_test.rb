fce5b7c540eb50bb7072f6c82b86c2b25a2c0ed0
Jon Jagger <jon@jaggersoft.com>
2017-03-13 16:32:35 +0000

    refactoring

--- client/test/src/pull_test.rb
+++ client/test/src/pull_test.rb
@@ -18,8 +18,8 @@ class PullerTest < TestBase
 
   test 'B22',
   'image_pulled?(valid and pulled image_name) is true' do
-    image_pull({image_name:'busybox'})
-    assert_equal true, image_pulled? 'busybox'
+    image_pull VALID_IMAGE_NAME
+    assert_equal true, image_pulled? VALID_IMAGE_NAME
   end
 =end
 
@@ -41,7 +41,7 @@ class PullerTest < TestBase
 
   test 'A82',
   'image_pull(valid and existing image_name) succeeds and returns true' do
-    assert_equal true, image_pull({image_name:'busybox'})
+    assert_equal true, image_pull VALID_IMAGE_NAME
   end
 
   # - - - - - - - - - - - - - - - - - - - - -
@@ -49,9 +49,9 @@ class PullerTest < TestBase
   test '667',
   'image_pull(valid non-existing image_name) raises' do
     error = assert_raises(StandardError) {
-      image_pull 'non_existent_box'
+      image_pull VALID_NON_EXISTENT_IMAGE_NAME
     }
-    expected = 'RunnerService:image_pull:command:docker pull non_existent_box'
+    expected = "RunnerService:image_pull:command:docker pull #{VALID_NON_EXISTENT_IMAGE_NAME}"
     assert_equal expected, error.message
   end
 =end
@@ -70,5 +70,7 @@ class PullerTest < TestBase
   private
 
   INVALID_IMAGE_NAME = '_cantStartWithSeparator'
+  VALID_NON_EXISTENT_IMAGE_NAME = 'non_existent_box'
+  VALID_IMAGE_NAME = 'busybox'
 
 end
