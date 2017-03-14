22fae75a5d26b0a7cd303a94dff8e6a8027e9ad6
Jon Jagger <jon@jaggersoft.com>
2017-03-14 18:22:17 +0000

    demo.sh now works

--- client/src/demo.rb
+++ client/src/demo.rb
@@ -16,19 +16,19 @@ class Demo < Sinatra::Base
     sss = nil
     html = '<div style="font-size:0.5em">'
 
-    duration = timed { sss = run({}) }
+    duration = timed { sss = run(files) }
     html += pre('run', duration, sss, 'Red')
 
-    syntax_error = { 'hiker.c' => 'sdsdsdsd' }
-    duration = timed { sss = run(syntax_error) }
+    files['hiker.c'] = 'sdsdsdsd'
+    duration = timed { sss = run(files) }
     html += pre('run', duration, sss, 'Yellow')
 
-    tests_run_and_pass = { 'hiker.c' => hiker_c.sub('6 * 9', '6 * 7') }
-    duration = timed { sss = run(tests_run_and_pass) }
+    files['hiker.c'] = hiker_c.sub('6 * 9', '6 * 7')
+    duration = timed { sss = run(files) }
     html += pre('run', duration, sss, 'Lime')
 
-    times_out = { 'hiker.c' => hiker_c.sub('return', "for(;;);\n    return") }
-    duration = timed { sss = run(times_out, 3) }
+    files['hiker.c'] = hiker_c.sub('return', "for(;;);\n    return")
+    duration = timed { sss = run(files, 3) }
     html += pre('run', duration, sss, 'LightGray')
 
     html += '</div>'
