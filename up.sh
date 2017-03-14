a008fd2757c2e29b11ed89cba8234b8ecdd01578
Jon Jagger <jon@jaggersoft.com>
2017-03-14 09:19:18 +0000

    up.sh now exits early if the server/client containers exit

--- up.sh
+++ up.sh
@@ -4,3 +4,31 @@ set -e
 my_dir="$( cd "$( dirname "${0}" )" && pwd )"
 docker-compose --file ${my_dir}/docker-compose.yml down
 docker-compose --file ${my_dir}/docker-compose.yml up -d
+
+sleep 1
+
+check_up()
+{
+  set +e
+  local s=$(docker ps --filter status=running --format '{{.Names}}' | grep ^${1}$)
+  set -e
+  if [ "${s}" != "${1}" ]; then
+    echo
+    echo "${1} exited"
+    docker logs ${1}
+    exit 1
+  fi
+}
+
+check_up 'runner_stateless_server'
+check_up 'runner_stateless_client'
+
+#set +e
+#c=$(docker ps --filter status=running --format '{{.Names}}' | grep ^runner_stateless_client$)
+#set -e
+#if [ "${c}" != "runner_stateless_client" ]; then
+#  echo
+#  echo "runner_stateless_client exited"
+#  docker logs runner_stateless_client
+#  exit 1
+#fi
