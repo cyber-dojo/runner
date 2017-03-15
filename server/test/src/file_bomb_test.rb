5e774248b65787e69bf0f3621f5eb61d1de5596e
Jon Jagger <jon@jaggersoft.com>
2017-03-15 05:35:31 +0000

    added file-bomb test

---
+++ server/test/src/file_bomb_test.rb
@@ -0,0 +1,46 @@
+require_relative 'test_base'
+require_relative 'os_helper'
+
+class FileBombTest < TestBase
+
+  include OsHelper
+
+  def self.hex_prefix; '1988B'; end
+
+  # - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+  test 'DB3',
+  '[Alpine] file() bomb in C to exhaust file-handles runs out of steam' do
+    gcc_assert_files['hiker.c'] =
+    [
+      '#include "hiker.h"',
+      '#include <stdio.h>',
+      '',
+      'int answer(void)',
+      '{',
+      '  for (int i = 0;;i++)',
+      '  {',
+      '    char filename[42];',
+      '    sprintf(filename, "wibble%d.txt", i);',
+      '    FILE * f = fopen(filename, "w");',
+      '    if (f)',
+      '      fprintf(stdout, "fopen() != NULL %s\n", filename);',
+      '    else',
+      '    {',
+      '      fprintf(stdout, "fopen() == NULL %s\n", filename);',
+      '      break;',
+      '    }',
+      '  }',
+      '  return 6 * 7;',
+      '}'
+    ].join("\n")
+    sss_run({ visible_files:gcc_assert_files })
+    assert_status success
+    assert_stderr ''
+    lines = stdout.split("\n")
+    assert_equal 1, lines.count{ |line| line == 'All tests passed' }
+    assert lines.count{ |line| line.start_with? 'fopen() != NULL' } > 42
+    assert_equal 1, lines.count{ |line| line.start_with? 'fopen() == NULL' }
+  end
+
+end
