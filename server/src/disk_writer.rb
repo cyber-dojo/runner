076c93dd3e580c75e0de5ff8302535ccc0a7d2a4
Jon Jagger <jon@jaggersoft.com>
2017-08-21 11:22:57 +0530

    refactoring; layout tweak

--- server/src/disk_writer.rb
+++ server/src/disk_writer.rb
@@ -5,7 +5,9 @@ class DiskWriter
   end
 
   def write(pathed_filename, content)
-    File.open(pathed_filename, 'w') { |fd| fd.write(content) }
+    File.open(pathed_filename, 'w') { |fd|
+      fd.write(content)
+    }
   end
 
 end
