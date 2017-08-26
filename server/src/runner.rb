4e803b6045f6d0e040ac5869c3b1803bfdbeb727
Jon Jagger <jon@jaggersoft.com>
2017-08-26 05:25:26 +0100

    drop unneeded -v in [mkdir -vp] command

--- server/src/runner.rb
+++ server/src/runner.rb
@@ -271,7 +271,7 @@ class Runner
         sub_dir = File.dirname(pathed_filename)
         src_dir = tmp_dir + '/' + sub_dir
         if sub_dir != '.'
-          shell.exec("mkdir -vp #{src_dir}")
+          shell.exec("mkdir -p #{src_dir}")
         end
         host_filename = tmp_dir + '/' + pathed_filename
         disk.write(host_filename, content)
