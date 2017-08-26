4d63fa8bf2e66bf3c3524c0e2ee6b4ede593c9ee
Jon Jagger <jon@jaggersoft.com>
2017-08-26 05:39:39 +0100

    added test for files being in sub-sub dirs

--- server/test/src/run_test.rb
+++ server/test/src/run_test.rb
@@ -69,12 +69,27 @@ class RunTest < TestBase
   test 'B82',
   %w( files can be in sub-dirs of sandbox ) do
     sss_run({ visible_files: {
-      'cyber-dojo.sh' => ls_cmd,
-      'a/hello.txt'   => 'hello world'
+      'a/hello.txt'   => 'hello world',
+      'cyber-dojo.sh' => ls_cmd
     }})
     ls_files = ls_parse(stdout)
-    salmon_uid = runner.user_id('salmon')
-    assert_equal_atts('a', 'drwxr-xr-x', salmon_uid, runner.group, 4096, ls_files)
+    uid = runner.user_id('salmon')
+    group = runner.group
+    assert_equal_atts('a', 'drwxr-xr-x', uid, group, 4096, ls_files)
+  end
+
+  # - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+  test 'B83',
+  %w( files can be in sub-sub-dirs of sandbox ) do
+    sss_run({ visible_files: {
+      'a/b/hello.txt'   => 'hello world',
+      'cyber-dojo.sh'   => "cd a && #{ls_cmd}"
+    }})
+    ls_files = ls_parse(stdout)
+    uid = runner.user_id('salmon')
+    group = runner.group
+    assert_equal_atts('b', 'drwxr-xr-x', uid, group, 4096, ls_files)
   end
 
   # - - - - - - - - - - - - - - - - - - - - - - - - - -
