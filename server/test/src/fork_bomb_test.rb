1f6832df7dbcb3d56f84f2695d04da8125f338ee
Jon Jagger <jon@jaggersoft.com>
2017-09-23 09:31:02 +0100

    refactoring; improve fork-bomb-from-shell test

--- server/test/src/fork_bomb_test.rb
+++ server/test/src/fork_bomb_test.rb
@@ -45,24 +45,26 @@ class ForkBombTest < TestBase
 
   test '4DE',
   %w( [Alpine] fork-bomb in shell fails to go off ) do
+    # The fork-bomb fails in a non-deterministic way.
+    # Occasionally, it throws an ArgumentError exception.
+    # The nocov markers around the block keep coverage at 100%
     @log = LoggerSpy.new(nil)
     cyber_dojo_sh = 'bomb() { bomb | bomb & }; bomb'
-    sss_run({ visible_files:{'cyber-dojo.sh' => cyber_dojo_sh }})
-    assert_status success
-    assert_stdout ''
-    assert_stderr_include "./cyber-dojo.sh: line 1: can't fork"
-    # Sometimes sss_run()throws an ArgumentError exception.
-    # This happens inside the attempt to read the file
-    # '/usr/local/bin/red_amber_green.rb' from the container.
-    # In this case the log is as below.
-    # This is a rare failure.
-    # How can I check for this and keep test coverage at 100%
-    #   rag_filename = '/usr/local/bin/red_amber_green.rb'
-    #   cmd = "'cat #{rag_filename}'"
-    #   assert /COMMAND:docker .* sh -c #{cmd}/.match @log.spied[1]
-    #   assert_equal 'STATUS:2',                      @log.spied[2]
-    #   assert_equal 'STDOUT:',                       @log.spied[3]
-    #   assert_equal "STDERR:sh: can't fork\n",       @log.spied[4]
+    # :nocov:
+    begin
+      sss_run({ visible_files:{'cyber-dojo.sh' => cyber_dojo_sh }})
+      assert_status success
+      assert_stdout ''
+      assert_stderr_include "./cyber-dojo.sh: line 1: can't fork"
+    rescue ArgumentError
+      rag_filename = '/usr/local/bin/red_amber_green.rb'
+      cmd = "'cat #{rag_filename}'"
+      assert /COMMAND:docker .* sh -c #{cmd}/.match @log.spied[1]
+      assert_equal 'STATUS:2',                      @log.spied[2]
+      assert_equal 'STDOUT:',                       @log.spied[3]
+      assert_equal "STDERR:sh: can't fork\n",       @log.spied[4]
+    end
+    # :nocov:
   end
 
 end
