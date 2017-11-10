066f6e553a8bceff0628a35b9aebf3797a1269b0
Jon Jagger <jon@jaggersoft.com>
2017-11-10 14:36:27 +0000

    move file-bomb test into multi-os-tests

--- server/test/src/multi_os_test.rb
+++ server/test/src/multi_os_test.rb
@@ -70,6 +70,21 @@ class MultiOSTest < TestBase
     }
   end
 
+  # - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+  multi_os_test 'DB3',
+  'file-bomb in C to exhaust file-handles fails to go off' do
+    filename = (os == :Alpine ? 'hiker.c' : 'hiker.cpp')
+    in_kata_as(salmon) {
+      run_cyber_dojo_sh({
+        changed_files: { filename => c_file_bomb }
+      })
+    }
+    assert seen?('All tests passed'), quad
+    assert seen?('fopen() != NULL'), quad
+    assert seen?('fopen() == NULL'), quad
+  end
+
   private
 
   # - - - - - - - - - - - - - - - - - - - - - - - - - -
@@ -345,6 +360,40 @@ class MultiOSTest < TestBase
     #  %y == time of last data modification <<=====
     #  %x == time of last access
     #  %w == time of file birth
+  end
+
+  # - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+  def seen?(text)
+    lines = (stdout+stderr).split("\n")
+    count = lines.count { |line| line.include?(text) }
+    count > 0
+  end
+
+  # - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+  def c_file_bomb
+    [
+      '#include <stdio.h>',
+      '',
+      'int answer(void)',
+      '{',
+      '  for (int i = 0;;i++)',
+      '  {',
+      '    char filename[42];',
+      '    sprintf(filename, "wibble%d.txt", i);',
+      '    FILE * f = fopen(filename, "w");',
+      '    if (f)',
+      '      fprintf(stdout, "fopen() != NULL %s\n", filename);',
+      '    else',
+      '    {',
+      '      fprintf(stdout, "fopen() == NULL %s\n", filename);',
+      '      break;',
+      '    }',
+      '  }',
+      '  return 6 * 7;',
+      '}'
+    ].join("\n")
   end
 
 end
