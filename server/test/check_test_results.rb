6fa3625a74f867b55aef1e436909ebd985447a61
Jon Jagger <jon@jaggersoft.com>
2017-11-10 14:47:31 +0000

    moved fork-momb tests into multi-os-tests

--- server/test/check_test_results.rb
+++ server/test/check_test_results.rb
@@ -106,7 +106,7 @@ table =
     [ 'coverage(test)[%]',      test_coverage,      '==', 100 ],
     [ 'hits_per_line(src)',     hits_per_line_src,  '<=',  86 ],
     [ 'hits_per_line(test)',    hits_per_line_test, '<=',  14 ],
-    [ 'lines(test)/lines(src)', f2(line_ratio),     '>=',   3 ],
+    [ 'lines(test)/lines(src)', f2(line_ratio),     '>=', 2.9 ],
   ]
 
 # - - - - - - - - - - - - - - - - - - - - - - -
--- server/test/src/multi_os_test.rb
+++ server/test/src/multi_os_test.rb
@@ -72,17 +72,23 @@ class MultiOSTest < TestBase
 
   # - - - - - - - - - - - - - - - - - - - - - - - - - -
 
+  multi_os_test 'CD5',
+  'fork-bomb does not run indefinitely' do
+    fork_bomb_test
+  end
+
+  # - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+  multi_os_test '4DE',
+  'shell fork-bomb does not run indefinitely' do
+    shell_fork_bomb_test
+  end
+
+  # - - - - - - - - - - - - - - - - - - - - - - - - - -
+
   multi_os_test 'DB3',
   'file-bomb in C to exhaust file-handles fails to go off' do
-    filename = (os == :Alpine ? 'hiker.c' : 'hiker.cpp')
-    in_kata_as(salmon) {
-      run_cyber_dojo_sh({
-        changed_files: { filename => c_file_bomb }
-      })
-    }
-    assert seen?('All tests passed'), quad
-    assert seen?('fopen() != NULL'), quad
-    assert seen?('fopen() == NULL'), quad
+    file_bomb_test
   end
 
   private
@@ -318,8 +324,6 @@ class MultiOSTest < TestBase
     assert_equal 5, count
   end
 
-  private
-
   def assert_stats(filename, permissions, size)
     stats = stdout_stats[filename]
     refute_nil stats, filename
@@ -364,13 +368,88 @@ class MultiOSTest < TestBase
 
   # - - - - - - - - - - - - - - - - - - - - - - - - - -
 
-  def seen?(text)
-    lines = (stdout+stderr).split("\n")
-    count = lines.count { |line| line.include?(text) }
-    count > 0
+  def fork_bomb_test
+    @log = LoggerSpy.new(nil)
+    filename = (os == :Alpine ? 'hiker.c' : 'hiker.cpp')
+    in_kata_as(salmon) {
+      begin
+        run_cyber_dojo_sh({
+          changed_files: { filename => fork_bomb_definition }
+        })
+        # :nocov:
+        assert_timed_out_or_printed 'All tests passed'
+        assert_timed_out_or_printed 'fork()'
+        # :nocov:
+      rescue ArgumentError
+      end
+    }
+  end
+
+  def fork_bomb_definition
+    [ '#include <stdio.h>',
+      '#include <unistd.h>',
+      '',
+      'int answer(void)',
+      '{',
+      '    for(;;)',
+      '    {',
+      '        int pid = fork();',
+      '        fprintf(stdout, "fork() => %d\n", pid);',
+      '        fflush(stdout);',
+      '        if (pid == -1)',
+      '            break;',
+      '    }',
+      '    return 6 * 7;',
+      '}'
+    ].join("\n")
   end
 
   # - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+  def shell_fork_bomb_test
+    @log = LoggerSpy.new(nil)
+    cant_fork = (os == :Alpine ? "can't fork" : 'Cannot fork')
+    in_kata_as(salmon) {
+      begin
+        shell_fork_bomb = [
+          'bomb()',
+          '{',
+          '   echo "bomb"',
+          '   bomb | bomb &',
+          '}',
+          'bomb'
+        ].join("\n")
+        run_cyber_dojo_sh({
+          changed_files: { 'cyber-dojo.sh' => shell_fork_bomb }
+        })
+        # :nocov:
+        assert_timed_out_or_printed 'bomb'
+        assert_timed_out_or_printed cant_fork
+        # :nocov:
+      rescue ArgumentError
+      end
+    }
+  end
+
+  def assert_timed_out_or_printed(text)
+    lines = (stdout+stderr).split("\n")
+    count = lines.count { |line| line.include?(text) }
+    assert (timed_out? || count > 0), ":#{text}:#{quad}:"
+  end
+
+  # - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+  def file_bomb_test
+    filename = (os == :Alpine ? 'hiker.c' : 'hiker.cpp')
+    in_kata_as(salmon) {
+      run_cyber_dojo_sh({
+        changed_files: { filename => c_file_bomb }
+      })
+    }
+    assert seen?('All tests passed'), quad
+    assert seen?('fopen() != NULL'), quad
+    assert seen?('fopen() == NULL'), quad
+  end
 
   def c_file_bomb
     [
@@ -394,6 +473,12 @@ class MultiOSTest < TestBase
       '  return 6 * 7;',
       '}'
     ].join("\n")
+  end
+
+  def seen?(text)
+    lines = (stdout+stderr).split("\n")
+    count = lines.count { |line| line.include?(text) }
+    count > 0
   end
 
 end
