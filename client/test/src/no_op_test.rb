d743e072d7b771f5f51dceef144513a6fcc4b1e3
Jon Jagger <jon@jaggersoft.com>
2017-11-12 06:30:55 +0000

    test refactoring

--- client/test/src/no_op_test.rb
+++ client/test/src/no_op_test.rb
@@ -1,9 +1,15 @@
 require_relative 'test_base'
 
-class AvatarTest < TestBase
+class NoOpTest < TestBase
 
   def self.hex_prefix
     '4F725'
+  end
+
+  multi_os_test 'D87',
+  %w( kata_new/kata_old are no-ops for API compatibility ) do
+    kata_new
+    kata_old
   end
 
   multi_os_test 'D08',
