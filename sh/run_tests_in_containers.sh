5fcbfc68d9ff413741747883182f4b01ca00dbd9
Jon Jagger <jon@jaggersoft.com>
2017-11-13 11:29:12 +0000

    [ci skip] dont dump logs at end of tests anymore

--- sh/run_tests_in_containers.sh
+++ sh/run_tests_in_containers.sh
@@ -42,14 +42,7 @@ if [[ ( ${server_status} == 0 && ${client_status} == 0 ) ]];  then
 else
   echo
   echo "server: cid = ${SERVER_CID}, status = ${server_status}"
-  if [ "${server_status}" != "0" ]; then
-    docker logs ${SERVER_CID}
-  fi
   echo "client: cid = ${CLIENT_CID}, status = ${client_status}"
-  if [ "${client_status}" != "0" ]; then
-    docker logs ${CLIENT_CID}
-    docker logs ${SERVER_CID}
-  fi
   echo
   exit 1
 fi
