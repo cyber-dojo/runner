5c5565a5a71ba148ec9bc906add6fa6b0eb1b28b
Jon Jagger <jon@jaggersoft.com>
2017-11-19 12:55:09 +0000

    refactoring

--- sh/docker_containers_up.sh
+++ sh/docker_containers_up.sh
@@ -3,21 +3,22 @@ set -e
 
 readonly ROOT_DIR="$( cd "$( dirname "${0}" )" && cd .. && pwd )"
 
-check_up()
+wait_till_up()
 {
-  set +e
-  local s=$(docker ps --filter status=running --format '{{.Names}}' | grep ^${1}$)
-  set -e
-  if [ "${s}" != "${1}" ]; then
-    echo
-    echo "${1} exited"
-    docker logs ${1}
-    exit 1
-  fi
+  local n=10
+  while [ $(( n -= 1 )) -ge 0 ]
+  do
+    if docker ps --filter status=running --format '{{.Names}}' | grep ^${1}$ ; then
+      return
+    else
+      sleep 0.5
+    fi
+  done
+  echo "${1} not up after 5 seconds"
+  docker logs ${1}
+  exit 1
 }
 
 docker-compose --file ${ROOT_DIR}/docker-compose.yml up -d
-# crude wait for services
-sleep 1
-check_up 'runner_stateless_server'
-check_up 'runner_stateless_client'
+wait_till_up 'runner_stateless_server'
+wait_till_up 'runner_stateless_client'
