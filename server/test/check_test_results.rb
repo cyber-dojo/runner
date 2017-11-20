5f744287d2a004f8f0f132754a64f333ae911ca5
Jon Jagger <jon@jaggersoft.com>
2017-11-20 19:41:38 +0000

    bump test metrics

--- server/test/check_test_results.rb
+++ server/test/check_test_results.rb
@@ -101,12 +101,12 @@ table =
     [ 'errors',                 error_count,        '==',   0 ],
     [ 'skips',                  skip_count,         '==',   0 ],
     [ 'assertions/s',           assertions_per_sec, '>=',  10 ],
-    [ 'duration(test)[s]',      test_duration,      '<=',  20 ],
+    [ 'duration(test)[s]',      test_duration,      '<=',  25 ],
     [ 'coverage(src)[%]',       src_coverage,       '==', 100 ],
     [ 'coverage(test)[%]',      test_coverage,      '==', 100 ],
-    [ 'hits_per_line(src)',     hits_per_line_src,  '<=',  47 ],
-    [ 'hits_per_line(test)',    hits_per_line_test, '<=', 7.9 ],
-    [ 'lines(test)/lines(src)', f2(line_ratio),     '>=', 2.5 ],
+    [ 'hits_per_line(src)',     hits_per_line_src,  '<=',  52 ],
+    [ 'hits_per_line(test)',    hits_per_line_test, '<=', 8.4 ],
+    [ 'lines(test)/lines(src)', f2(line_ratio),     '>=', 2.6 ],
   ]
 
 # - - - - - - - - - - - - - - - - - - - - - - -
