4cccd8ffc21d0f0c7004c6459911aee6dac148e4
Jon Jagger <jon@jaggersoft.com>
2017-11-27 15:53:08 +0000

    refactoring; shell.exec(mkdir -p) now shell.assert(mkdir -p)

--- server/src/runner.rb
+++ server/src/runner.rb
@@ -96,7 +96,7 @@ class Runner # stateless
       sub_dir = File.dirname(pathed_filename)
       unless sub_dir == '.'
         src_dir = tmp_dir + '/' + sub_dir
-        shell.exec("mkdir -p #{src_dir}")
+        shell.assert("mkdir -p #{src_dir}")
       end
       src_filename = tmp_dir + '/' + pathed_filename
       disk.write(src_filename, content)
