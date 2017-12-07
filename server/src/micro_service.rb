bb96d5d5962451cd02f4f6a42cf4362018b192fa
Jon Jagger <jon@jaggersoft.com>
2017-12-07 07:19:53 +0000

    refactoring

--- server/src/micro_service.rb
+++ server/src/micro_service.rb
@@ -21,12 +21,30 @@ class MicroService
   end
 
   def call(env, request = Rack::Request.new(env))
-    @name = request.path_info[1..-1] # lose leading /
-    @json_args = json_args(request)
-    @args = case @name
-      when /^image_pulled$/
-        @name += '?'
-        []
+    name, args = validated_name_args(request)
+    runner = Runner.new(self, image_name, kata_id)
+    result = runner.public_send(name, *args)
+    body = { name => result }
+    if ledger.key?('red_amber_green')
+      body['red_amber_green'] = ledger['red_amber_green']
+    end
+    triple(body)
+  rescue ShellError => error
+    triple({ 'exception' => error.args })
+  rescue => error
+    triple({ 'exception' => error.message })
+  end
+
+  private # = = = = = = = = = = = =
+
+  def validated_name_args(request)
+    name = request.path_info[1..-1] # lose leading /
+    @json_args = json_parse(request.body.read)
+    unless @json_args.is_a?(Hash)
+      raise 'json:!Hash'
+    end
+    args = case name
+      when /^image_pulled$/ then []
       when /^image_pull$/   then []
       when /^kata_new$/     then []
       when /^kata_old$/     then []
@@ -37,32 +55,8 @@ class MicroService
          new_files, deleted_files, unchanged_files, changed_files,
          max_seconds]
     end
-    runner = Runner.new(self, image_name, kata_id)
-    result = runner.public_send(@name, *@args)
-    body = { @name => result }
-    if ledger.key?('red_amber_green')
-      body['red_amber_green'] = ledger['red_amber_green']
-    end
-
-    [ 200, header_content_is_json, [ body.to_json ] ]
-
-  rescue ShellError => error
-    body = { 'exception' => error.args }
-    [ 200, header_content_is_json, [ body.to_json ] ]
-
-  rescue => error
-    body = { 'exception' => error.message }
-    [ 200, header_content_is_json, [ body.to_json ] ]
-  end
-
-  private # = = = = = = = = = = = =
-
-  def json_args(request)
-    args = json_parse(request.body.read)
-    unless args.is_a?(Hash)
-      raise 'json:!Hash'
-    end
-    args
+    name += '?' if name == 'image_pulled'
+    [name, args]
   end
 
   def json_parse(request)
@@ -71,8 +65,8 @@ class MicroService
     raise 'json:invalid'
   end
 
-  def header_content_is_json
-    { 'Content-Type' => 'application/json' }
+  def triple(body)
+    [ 200, { 'Content-Type' => 'application/json' }, [ body.to_json ] ]
   end
 
   # - - - - - - - - - - - - - - - -
@@ -80,34 +74,16 @@ class MicroService
   # - - - - - - - - - - - - - - - -
 
   def image_name
-    arg = @json_args[__method__.to_s]
-    unless valid_image_name?(arg)
-      raise invalid('image_name')
-    end
-    arg
+    validated_image_name
   end
-
-  # - - - - - - - - - - - - - - - -
 
   def kata_id
-    arg = @json_args[__method__.to_s]
-    unless valid_kata_id?(arg)
-      raise invalid('kata_id')
-    end
-    arg
+    validated_kata_id
   end
-
-  # - - - - - - - - - - - - - - - -
 
   def avatar_name
-    arg = @json_args[__method__.to_s]
-    unless valid_avatar_name?(arg)
-      raise invalid('avatar_name')
-    end
-    arg
+    validated_avatar_name
   end
-
-  # - - - - - - - - - - - - - - - -
 
   def starting_files
     validated_files(__method__)
@@ -129,21 +105,33 @@ class MicroService
     validated_files(__method__)
   end
 
-  # - - - - - - - - - - - - - - - -
-
   def max_seconds
-    arg = @json_args[__method__.to_s]
-    unless valid_max_seconds?(arg)
-      raise invalid('max_seconds')
-    end
-    arg
+    validated_max_seconds
   end
 
   # - - - - - - - - - - - - - - - -
   # validations
   # - - - - - - - - - - - - - - - -
 
+  def validated_image_name
+    arg = @json_args['image_name']
+    unless valid_image_name?(arg)
+      raise invalid('image_name')
+    end
+    arg
+  end
+
   include ValidImageName
+
+  # - - - - - - - - - - - - - - - -
+
+  def validated_kata_id
+    arg = @json_args['kata_id']
+    unless valid_kata_id?(arg)
+      raise invalid('kata_id')
+    end
+    arg
+  end
 
   def valid_kata_id?(kata_id)
     kata_id.is_a?(String) &&
@@ -153,6 +141,16 @@ class MicroService
 
   def hex?(char)
     '0123456789ABCDEF'.include?(char)
+  end
+
+  # - - - - - - - - - - - - - - - -
+
+  def validated_avatar_name
+    arg = @json_args['avatar_name']
+    unless valid_avatar_name?(arg)
+      raise invalid('avatar_name')
+    end
+    arg
   end
 
   def valid_avatar_name?(avatar_name)
@@ -170,9 +168,21 @@ class MicroService
     arg
   end
 
+  # - - - - - - - - - - - - - - - -
+
   def valid_files?(arg)
     arg.is_a?(Hash) &&
       arg.all? { |k,v| k.is_a?(String) && v.is_a?(String) }
+  end
+
+  # - - - - - - - - - - - - - - - -
+
+  def validated_max_seconds
+    arg = @json_args['max_seconds']
+    unless valid_max_seconds?(arg)
+      raise invalid('max_seconds')
+    end
+    arg
   end
 
   def valid_max_seconds?(arg)
