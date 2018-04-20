409e77f32172f943e84be4dd0d5007a1f32a1311
Jon Jagger <jon@jaggersoft.com>
2018-04-20 06:38:20 +0100

    undo non-fixing fix to tear_down.sh script

--- sh/tear_down.sh
+++ sh/tear_down.sh
@@ -5,7 +5,7 @@
 # I can shell into it (eg for debugging). So I don't do a teardown at the
 # end of each test. Instead I do a big teardown before all the tests run.
 
-readonly PREFIX='test_runner_stateless_'
+readonly PREFIX='test_run__runner_stateless_'
 
 readonly ZOMBIE_CONTAINERS=$(docker ps --all --filter "name=${PREFIX}" --format "{{.Names}}")
 if [ "${ZOMBIE_CONTAINERS}" != "" ]; then
