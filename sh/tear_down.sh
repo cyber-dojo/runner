b615e2bd33cd24b89a43968b6220032b99d4b7f7
Jon Jagger <jon@jaggersoft.com>
2018-04-20 08:23:55 +0100

    refactoring; comment update

--- sh/tear_down.sh
+++ sh/tear_down.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
-# During development, when a test fails it can leave containers around.
-# I'd like to leave the container for a failed test un-removed since that way
+# During development, when a test fails it can leave a test-framework
+# containers around. I leave such containers un-removed since that way
 # I can shell into it (eg for debugging). So I don't do a teardown at the
 # end of each test. Instead I do a big teardown before all the tests run.
 
 readonly PREFIX='test_run__runner_stateless_'
 
 readonly ZOMBIE_CONTAINERS=$(docker ps --all --filter "name=${PREFIX}" --format "{{.Names}}")
+
 if [ "${ZOMBIE_CONTAINERS}" != "" ]; then
   docker rm --force ${ZOMBIE_CONTAINERS}
 fi
