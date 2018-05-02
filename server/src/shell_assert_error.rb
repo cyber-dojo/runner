c014563c697e41ddf1cce6dee2441486137bafdc
Jon Jagger <jon@jaggersoft.com>
2018-05-02 20:07:44 +0100

    refactoring; delete dead code

--- server/src/shell_assert_error.rb
+++ server/src/shell_assert_error.rb
@@ -11,19 +11,4 @@ class ShellAssertError < StandardError
     }))
   end
 
-=begin
-  if error
-      set_backtrace(error.backtrace)
-      @message = {
-        'command' => command,
-        'error' => error.message,
-      }.to_json
-    else
-      @message = command
-    end
-  end
-
-  attr_reader :message
-=end
-
 end
