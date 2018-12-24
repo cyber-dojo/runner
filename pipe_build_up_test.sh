57558375033d06b465f30dcfa9dd2f88ec3bf340
Jon Jagger <jon@jaggersoft.com>
2018-12-24 17:27:04 +0000

    refactoring; simplify pipe-build-up-test script

--- pipe_build_up_test.sh
+++ pipe_build_up_test.sh
@@ -2,14 +2,10 @@
 set -e
 
 readonly ROOT_DIR="$( cd "$( dirname "${0}" )" && pwd )"
-readonly MY_NAME=runner-stateless
 readonly SH_DIR="${ROOT_DIR}/sh"
 
 "${SH_DIR}/build_docker_images.sh"
 "${SH_DIR}/docker_containers_up.sh"
 "${SH_DIR}/tear_down.sh"
-if "${SH_DIR}/run_tests_in_containers.sh" "$@"; then
-  "${SH_DIR}/docker_containers_down.sh"
-  docker rmi "cyberdojo/${MY_NAME}-client" > /dev/null 2>&1
-  #docker image prune --force > /dev/null 2>&1
-fi
+"${SH_DIR}/run_tests_in_containers.sh" "$@"
+"${SH_DIR}/docker_containers_down.sh"
