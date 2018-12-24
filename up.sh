15150a8b5c7f66a62a6d97b45e4294daaffd3b28
Jon Jagger <jon@jaggersoft.com>
2018-12-24 17:10:44 +0000

    ensure porter's docker logs are clean on startup

--- up.sh
+++ up.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 set -e
 
-# Note that the --host is needed for IPv4 and IPv6 addresses
+# HOME=/tmp is needed to avoid warnings such as
+#   Bundler will use `/tmp/bundler/home/unknown' as your home directory temporarily
+# This is important for tests that assert on container log contents at startup.
 
-bundle exec rackup \
-  --warn \
-  --host 0.0.0.0 \
-  --port 4597 \
-  --server thin \
+# --host is needed for IPv4 and IPv6 addresses
+
+HOME=/tmp bundle exec rackup \
+  --warn           \
+  --host 0.0.0.0   \
+  --port 4597      \
+  --server thin    \
   --env production \
     config.ru
