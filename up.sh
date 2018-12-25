34ec51981c6e1692603c69b28d24bf06771991c1
Jon Jagger <jon@jaggersoft.com>
2018-12-25 08:37:49 +0000

    refactoring; drop unneeded [bundle exec] from up.sh

--- up.sh
+++ up.sh
@@ -1,16 +1,9 @@
 #!/bin/bash
-set -e
 
-# HOME=/tmp is needed to avoid warnings such as
-#   Bundler will use `/tmp/bundler/home/unknown' as your home directory temporarily
-# This is important for tests that assert on container log contents at startup.
-
-# --host is needed for IPv4 and IPv6 addresses
-
-HOME=/tmp bundle exec rackup \
-  --warn           \
+rackup             \
+  --env production \
   --host 0.0.0.0   \
   --port 4597      \
   --server thin    \
-  --env production \
+  --warn           \
     config.ru
