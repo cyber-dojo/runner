7887eaad2cb00555f09239e6c139fcbae75feae8
Jon Jagger <jon@jaggersoft.com>
2019-05-05 10:42:53 +0100

    reduce max time for test-runs

--- test_client/test/util/check_test_results.rb
+++ test_client/test/util/check_test_results.rb
@@ -94,7 +94,7 @@ table =
     [ 'failures',               failure_count,  '==',   0 ],
     [ 'errors',                 error_count,    '==',   0 ],
     [ 'skips',                  skip_count,     '==',   0 ],
-    [ 'duration(test)[s]',      test_duration,  '<=',  40 ],
+    [ 'duration(test)[s]',      test_duration,  '<=',  20 ],
     [ 'coverage(src)[%]',       src_coverage,   '==', 100 ],
     [ 'coverage(test)[%]',      test_coverage,  '==', 100 ],
     [ 'lines(test)/lines(src)', f2(line_ratio), '>=', 4.4 ],
