2b51051e87ad11bf33cd81fa26a71a27a2c95737
Jon Jagger <jon@jaggersoft.com>
2019-05-30 23:38:36 +0100

    [ci skip] fix incorrect container name prefix in tear-down helper script

--- sh/tear_down.sh
+++ sh/tear_down.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-# During development, when a test fails it can leave a test-framework
+# During development, when a test fails it can very occasionally leave a
 # containers around. I leave such containers un-removed since that way
 # I can shell into it (eg for debugging). So I don't do a teardown at the
 # end of each test. Instead I do a big teardown before all the tests run.
 
-readonly PREFIX='test_run__runner'
+readonly PREFIX='cyber_dojo_runner'
 
 readonly ZOMBIE_CONTAINERS=$(docker ps --all --filter "name=${PREFIX}" --format "{{.Names}}")
 
