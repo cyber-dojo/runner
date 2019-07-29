7e5ca686ab7801af66682936c595c1c7c25c2809
JonJagger <jon@jaggersoft.com>
2019-07-29 06:57:06 +0100

    tag locally built runner image so you can more easily run local integration tests

--- sh/build_docker_images.sh
+++ sh/build_docker_images.sh
@@ -4,14 +4,20 @@ set -e
 readonly ROOT_DIR="$( cd "$( dirname "${0}" )" && cd .. && pwd )"
 export SHA=$(cd "${ROOT_DIR}" && git rev-parse HEAD)
 
-echo
-docker-compose \
-  --file "${ROOT_DIR}/docker-compose.yml" \
-    build \
-      runner-server
+build_service_image()
+{
+  echo
+  docker-compose \
+    --file "${ROOT_DIR}/docker-compose.yml" \
+      build \
+        "${1}"
+}
 
-echo
-docker-compose \
-  --file "${ROOT_DIR}/docker-compose.yml" \
-    build \
-      runner-client
+build_service_image runner-server
+build_service_image runner-client
+
+# Assuming we do not have any new runner commits, runner's latest commit
+# sha will match the image tag inside versioner's .env file.
+# This means we can tag to it and a [cyber-dojo up] call
+# will use the tagged image.
+docker tag cyberdojo/runner:latest cyberdojo/runner:${SHA:0:7}
