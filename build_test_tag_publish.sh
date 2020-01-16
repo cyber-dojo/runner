a74c5bcbb43e8fcf497be997809ce1951979e7a0
Jon Jagger <jon@jaggersoft.com>
2020-01-16 10:20:06 +0000

    tag images earlier in build sequence for fast local integration testing

--- build_test_tag_publish.sh
+++ build_test_tag_publish.sh
@@ -14,11 +14,11 @@ readonly SH_DIR="$( cd "$( dirname "${0}" )/sh" && pwd )"
 source ${SH_DIR}/versioner_env_vars.sh
 export $(versioner_env_vars)
 ${SH_DIR}/build_docker_images.sh
+${SH_DIR}/tag_image.sh
 ${SH_DIR}/tear_down.sh
 ${SH_DIR}/docker_containers_up.sh
 ${SH_DIR}/on_ci_pull_dependent_images.sh
 ${SH_DIR}/test_in_containers.sh "$@"
 ${SH_DIR}/docker_containers_down.sh
-${SH_DIR}/tag_image.sh
 ${SH_DIR}/on_ci_publish_tagged_images.sh
 #${SH_DIR}/trigger_dependent_images.sh
