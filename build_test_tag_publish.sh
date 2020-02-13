2c404df8aae865997bb1c63a01cd5cfbb919098e
Jon Jagger <jon@jaggersoft.com>
2020-02-13 22:10:07 +0000

    Simplify build-script help text

--- build_test_tag_publish.sh
+++ build_test_tag_publish.sh
@@ -1,12 +1,15 @@
-#!/bin/bash -Ee
+#!/bin/bash -Eeu
 
-if [ "${1}" == '-h' ] || [ "${1}" == '--help' ]; then
+readonly MY_NAME=`basename "${0}"`
+
+if [ "${1:-}" == '-h' ] || [ "${1:-}" == '--help' ]; then
   echo
-  echo 'Use: build_test_tag_publish.sh [client|server] [ID...]'
+  echo "Use: ${MY_NAME} [client|server] [ID...]"
   echo 'Options:'
   echo '   client  - only run the tests from inside the client'
   echo '   server  - only run the tests from inside the server'
   echo '   ID...   - only run the tests matching these identifiers'
+  echo
   exit 0
 fi
 
