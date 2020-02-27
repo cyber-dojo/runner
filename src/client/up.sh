a61c828b8b452b4dbb9d763d30c475018254e5c9
Jon Jagger <jon@jaggersoft.com>
2020-02-27 16:55:22 +0000

    Tidy up.sh file

--- src/client/up.sh
+++ src/client/up.sh
@@ -1,11 +1,12 @@
 #!/bin/bash -Eeu
 
-# Note that the --host is needed for IPv4 and IPv6 addresses
+readonly MY_DIR="$( cd "$(dirname "${0}")" && pwd )"
+
+export RUBYOPT='-W2'
 
 rackup \
   --warn \
-  --host 0.0.0.0 \
   --port ${PORT} \
   --server thin \
   --env production \
-    /app/config.ru
+    ${MY_DIR}/config.ru
