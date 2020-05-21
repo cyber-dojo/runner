ca0b9aaab03056ba9d547ad5165a62cec15966b2
Jon Jagger <jon@jaggersoft.com>
2020-05-21 20:44:49 +0100

    Delete dead Log class

--- src/server/code/externals.rb
+++ src/server/code/externals.rb
@@ -1,6 +1,5 @@
 # frozen_string_literal: true
 require_relative 'bash'
-require_relative 'log'
 require_relative 'string_logger'
 require_relative 'traffic_light'
 require_relative 'rag_lambdas'
@@ -13,11 +12,9 @@ class Externals
     @bash = options[:bash] || Bash.new(self)
     @rag_lambdas = options[:rag_lambdas] || RagLambdas.new
     @traffic_light = options[:traffic_light] || TrafficLight.new(self)
-
-    @log = options['log'] || Log.new
   end
 
-  attr_reader :bash, :log, :logger
+  attr_reader :bash, :logger
   attr_reader :rag_lambdas, :traffic_light
 
 end
