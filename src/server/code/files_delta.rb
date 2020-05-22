807ba84e4ab3e4f3f2e1f6d52a95271d22e6e51c
Jon Jagger <jon@jaggersoft.com>
2020-05-22 19:51:04 +0100

    [ci skip] Add comment to files_delta()

--- src/server/code/files_delta.rb
+++ src/server/code/files_delta.rb
@@ -2,7 +2,7 @@
 module FilesDelta
 
   # files_in (old)
-  # Format == { "hiker.c" => "#include..." } Never truncated
+  # Format == { "hiker.c" => "#include..." }
   # files_out (new)
   # Format == { "hiker.c" => { content: "#include...", truncated: false } }
 
@@ -19,5 +19,11 @@ module FilesDelta
     end
     [ created=new, deleted, changed ]
   end
+
+  # The old files are assumed to NOT be truncated.
+  # To check for a changed file we only have to check the
+  # new files' content. If the new file has been truncated
+  # then the content must have changed since the old files
+  # are assumed NON truncated.
 
 end
