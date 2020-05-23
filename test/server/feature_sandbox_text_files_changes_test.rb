0e8fc34ed8a5380eaa21632d557ee6eddb3ab50c
Jon Jagger <jon@jaggersoft.com>
2020-05-23 08:04:00 +0100

    Put tests for detecting changes to text files in /sandbox into one file

--- test/server/feature_sandbox_text_files_changes_test.rb
+++ test/server/feature_sandbox_text_files_changes_test.rb
@@ -1,16 +1,131 @@
 # frozen_string_literal: true
 require_relative 'test_base'
 
-class FeatureSandboxSubDirTest < TestBase
+class FeatureSandboxTextFilesChangesTest < TestBase
 
   def self.id58_prefix
-    'D8D'
+    'ECF'
+  end
+
+  # - - - - - - - - - - - - - - - - -
+
+  multi_os_test '524', %w(
+  created text files are returned when
+  their names have leading hyphens which must not
+  be read as a tar-pipe option
+  ) do
+    leading_hyphen = '-JPlOLNY7yt_fFndapHwIg'
+    script = "printf 'xxx' > '#{leading_hyphen}';"
+    assert_sss(script)
+    assert_created({leading_hyphen => intact('xxx')})
+    assert_deleted([])
+    assert_changed({})
+  end
+
+  # - - - - - - - - - - - - - - - - -
+
+  multi_os_test '526', %w(
+  created text files, including dot files, are returned
+  ) do
+    assert_sss([
+      'printf "xxx" > newfile.txt',
+      'printf "yyy" > .dotfile'
+    ].join(';'))
+    assert_created({
+      'newfile.txt' => intact('xxx'),
+      '.dotfile' => intact('yyy')
+    })
+    assert_deleted([])
+    assert_changed({})
+  end
+
+  # - - - - - - - - - - - - - - - - -
+
+  multi_os_test '527', %w(
+  created binary files are not returned
+  ) do
+    script = [
+      'dd if=/dev/zero of=binary.dat bs=1c count=42',
+      'file --mime-encoding binary.dat'
+    ].join(';')
+    assert_sss(script)
+    assert stdout.include?('binary.dat: binary')
+    assert_created({})
+    assert_deleted([])
+    assert_changed({})
+  end
+
+  # - - - - - - - - - - - - - - - - -
+
+  multi_os_test '530', %w(
+  deleted text filenames are returned
+  ) do
+    filename = any_src_file
+    script = "rm #{filename}"
+    assert_sss(script)
+    assert_created({})
+    assert_deleted([filename])
+    assert_changed({})
+  end
+
+  # - - - - - - - - - - - - - - - - -
+
+  multi_os_test '531', %w(
+  changed text files are returned
+  ) do
+    filename = any_src_file
+    content = 'XXX'
+    script = "printf '#{content}' > #{filename}"
+    assert_sss(script)
+    assert_created({})
+    assert_deleted([])
+    assert_changed({filename => intact(content)})
+  end
+
+  # - - - - - - - - - - - - - - - - -
+
+  multi_os_test '532', %w(
+  empty new text files are returned
+  ) do
+    # The file utility says empty files are binary files!
+    filename = 'empty.txt'
+    script = "touch #{filename}"
+    assert_sss(script)
+    assert_created({filename => intact('')})
+    assert_deleted([])
+    assert_changed({})
+  end
+
+  # - - - - - - - - - - - - - - - - -
+
+  multi_os_test '533', %w(
+  single-char new text files are returned
+  ) do
+    # The file utility says single-char files are binary files!
+    filename = 'one-char.txt'
+    ch = 'x'
+    script = "printf '#{ch}' > #{filename}"
+    assert_sss(script)
+    assert_created({filename => intact(ch)})
+    assert_deleted([])
+    assert_changed({})
+  end
+
+  # - - - - - - - - - - - - - - - - -
+
+  multi_os_test '62C', %w(
+  no text files under /sandbox at all, returns everything deleted
+  ) do
+    assert_sss('rm -rf /sandbox/* /sandbox/.*')
+    assert_created({})
+    assert_deleted(manifest['visible_files'].keys.sort)
+    assert_changed({})
   end
 
   # - - - - - - - - - - - - - - - - -
 
   multi_os_test '12A',
-  'browser can create files in sandbox/ sub-dirs' do
+  %w( create text files in /sandbox sub-dirs are returned ) do
     # The tar-pipe handles creating dir structure
     assert_browser_can_create_files_in_sandbox_sub_dir('s1')
     assert_browser_can_create_files_in_sandbox_sub_dir('s1/s2')
@@ -19,7 +134,7 @@ class FeatureSandboxSubDirTest < TestBase
   # - - - - - - - - - - - - - - - - - - - - - - - - - -
 
   multi_os_test '12B',
-  'cyber-dojo.sh can create files in sandbox/ sub-dirs' do
+  %w( created text files in /sandbox sub-dirs are returned ) do
     assert_cyber_dojo_sh_can_create_files_in_sandbox_sub_dir('d1')
     assert_cyber_dojo_sh_can_create_files_in_sandbox_sub_dir('d1/d2/d3')
   end
@@ -27,12 +142,12 @@ class FeatureSandboxSubDirTest < TestBase
   # - - - - - - - - - - - - - - - - - - - - - - - - - -
 
   multi_os_test '12C',
-  %w( cyber-dojo.sh can delete files from sandbox/ sub-dir ) do
+  %w( deleted text filenames from /sandbox sub-dir are returned ) do
     assert_cyber_dojo_sh_can_delete_files_from_sandbox_sub_dir('c1')
     assert_cyber_dojo_sh_can_delete_files_from_sandbox_sub_dir('c1/c2/c3')
   end
 
-  private # = = = = = = = = = = = = = = = = = = = = = =
+  private
 
   def assert_browser_can_create_files_in_sandbox_sub_dir(sub_dir)
     filename = 'hello.txt'
@@ -118,6 +233,14 @@ class FeatureSandboxSubDirTest < TestBase
          time_stamp: attr[6],      # eg 07:03:14.539952547
       }]
     }.to_h
+  end
+
+  # - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+  def any_src_file
+    manifest['visible_files'].keys.find do |filename|
+      filename.split('.')[0].upcase === 'HIKER'
+    end
   end
 
 end
