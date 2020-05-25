59b2eb0a2846f5f6c07a6a7431217241c6651e6a
Jon Jagger <jon@jaggersoft.com>
2020-05-25 10:30:10 +0100

    [ci skip] tweak prepared shell files

--- src/server/code/parked/shell-files/home_sandbox_main.sh
+++ src/server/code/parked/shell-files/home_sandbox_main.sh
@@ -7,7 +7,7 @@ function send_tgz()
   truncate_file "${TMP_DIR}/stdout"
   truncate_file "${TMP_DIR}/stderr"
   tar -rf "${TAR_FILE}" -C "${TMP_DIR}" stdout stderr status
-  text_filenames | tar -C / -rf ${TAR_FILE} -T -
+  text_filenames | tar -C / -rf ${TAR_FILE} --verbatim-files-from -T -
   zip -c "${TAR_FILE}"
 }
 
@@ -20,8 +20,9 @@ function text_filenames()
 function is_truncated_text_file()
 {
   filename="${1}"
-  if is_text_file "${filename}"; then
-    truncate_file "${filename}"
+  size=$(stat -c%s "${filename}")
+  if is_text_file "${filename}" "${size}"; then
+    truncate_file "${filename}" "${size}"
     true
   else
     false
@@ -30,10 +31,13 @@ function is_truncated_text_file()
 
 function is_text_file()
 {
+  # grep -q is --quiet, we are generating text file names.
+  # grep -v is --invert-match.
   filename="${1}"
-  if file --mime-encoding ${filename} | grep -qv "${1}:\\sbinary"; then
+  size="${2}"
+  if file --mime-encoding ${filename} | grep -qv "${filename}:\\sbinary"; then
     true
-  elif [ $(stat -c%s "${filename}") -lt 2 ]; then
+  elif [ "${size}" -lt 2 ]; then
     # file incorrectly reports very small text files as binary.
     true
   else
@@ -46,8 +50,9 @@ function truncate_file()
   # Beware; truncate can extend OR strink the size of a file.
   # The +1 is so Ruby can detect and lop off the final extra byte.
   filename="${1}"
-  if [ $(stat -c%s "${filename}") -gt #{MAX_FILE_SIZE} ]; then
-    truncate --size #{MAX_FILE_SIZE+1} "${filename}"
+  size="${2}"
+  if [ "${size}" -gt ${MAX_FILE_SIZE} ]; then
+    truncate --size ${MAX_FILE_SIZE+1} "${filename}"
   fi
 }
 
