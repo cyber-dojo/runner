0c5cea4e9416dc57c3a9a2bc9b960b6f73856125
Jon Jagger <jon@jaggersoft.com>
2020-05-31 21:22:09 +0100

    Update content that will become top of cyber-dojo.sh files

--- docs/scratch/top_of_cyber_dojo.sh
+++ docs/scratch/top_of_cyber_dojo.sh
@@ -1,24 +1,24 @@
 set -e
 
 # --------------------------------------------------------------
-# Text files under /sandbox are automatically returned.
+# Text files under /sandbox are automatically returned...
 source ~/cyber_dojo_delete_dirs.sh
 source ~/cyber_dojo_delete_files.sh
 source ~/cyber_dojo_reset_dirs.sh
 export REPORT_DIR=${CYBER_DOJO_SANDBOX}/report
 function cyber_dojo_enter()
 {
-  # Only return _newly_ generated reports.
+  # 1. Only return _newly_ generated reports.
   cyber_dojo_reset_dirs ${REPORT_DIR}
 }
 function cyber_dojo_exit()
 {
-  # Remove text files we don't want returned.
+  # 2. Remove text dirs/files we don't want returned.
   cyber_dojo_delete_dirs .pytest_cache # ...
   #cyber_dojo_delete_files ...
 }
 cyber_dojo_enter
-trap cyber_dojo_exit EXIT SIGTERM
+trap cyber_dojo_exit EXIT SIGTERM SIGKILL
 # --------------------------------------------------------------
 
 
