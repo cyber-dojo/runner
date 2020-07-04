daa5fdc433230759b7dd60f1dda0fb7ee0a41d53
Jon Jagger <jon@jaggersoft.com>
2020-07-04 13:48:41 +0100

    Start client-side puma server directly in up.sh

--- source/client/up.sh
+++ source/client/up.sh
@@ -1,12 +1,9 @@
 #!/bin/bash -Eeu
 
-readonly MY_DIR="$(cd "$(dirname "${0}")" && pwd)"
+readonly MY_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 export RUBYOPT='-W2'
 
-rackup \
-  --env production \
-  --port ${PORT}   \
-  --server thin    \
-  --warn           \
-    ${MY_DIR}/config.ru
+puma \
+  --port=${PORT} \
+  --config=${MY_DIR}/puma.rb
