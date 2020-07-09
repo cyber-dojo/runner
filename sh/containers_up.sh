f4ad6629dc67c14ad9dd48ea2349ee6ef5957bdd
Jon Jagger <jon@jaggersoft.com>
2020-07-09 07:39:57 +0100

    Delete unneeded statement

--- sh/containers_up.sh
+++ sh/containers_up.sh
@@ -124,5 +124,4 @@ docker-compose \
   -d \
   --force-recreate
 
-wait_until_ready_and_clean test-runner-server "${CYBER_DOJO_RUNNER_PORT}"
 wait_until_ready_and_clean test-runner-client "${CYBER_DOJO_RUNNER_CLIENT_PORT}"
