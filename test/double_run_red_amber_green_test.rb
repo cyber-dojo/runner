60c47259bb6c6b6cb325f1e64b6beec944749413
Jon Jagger <jon@jaggersoft.com>
2020-07-24 12:21:06 +0100

    Added server-side test for run red/amber/green

--- test/double_run_red_amber_green_test.rb
+++ test/double_run_red_amber_green_test.rb
@@ -8,6 +8,57 @@ class DoubleRunRedAmberGreenTest < TestBase
     'c7B'
   end
 
+  # - - - - - - - - - - - - - - - - -
+
+  test 'd56', %w( red ) do
+    on_server_stub(CSHARP_NUNIT_STDERR[:red])
+    run_cyber_dojo_sh
+    assert red?, run_result
+  end
+
+  # - - - - - - - - - - - - - - - - -
+
+  test 'd57', %w( amber ) do
+    on_server_stub(CSHARP_NUNIT_STDERR[:amber])
+    run_cyber_dojo_sh
+    assert amber?, run_result
+  end
+
+  # - - - - - - - - - - - - - - - - -
+
+  test 'd58', %w( green ) do
+    on_server_stub(CSHARP_NUNIT_STDERR[:green])
+    run_cyber_dojo_sh
+    assert green?, run_result
+  end
+
+  private
+
+  CSHARP_NUNIT_STDERR = {
+      red:'Test Count: 3, Passed: 2, Failed: 1, Warnings: 0, Inconclusive: 0, Skipped: 0',
+    amber:'sdfgsdfgsdfgsdfg',
+    green:'Test Count: 3, Passed: 3, Failed: 0, Warnings: 0, Inconclusive: 0, Skipped: 0'
+  }
+
+  # - - - - - - - - - - - - - - - - -
+
+  def on_server_stub(mx_stderr)
+    if ENV['CONTEXT'] === 'server'
+      stdout_tgz = TGZ.of({'stderr' => mx_stderr})
+      stderr = ''
+      @context = Context.new(
+        logger:StdoutLoggerSpy.new,
+        process:process=ProcessAdapterStub.new,
+        threader:StdoutStderrThreaderStub.new(stdout_tgz, stderr)
+      )
+      puller.add(image_name)
+      tp = ProcessAdapter.new
+      process.spawn { |_cmd,opts| tp.spawn('sleep 10', opts) }
+      process.detach { |pid| tp.detach(pid); ThreadStub.new(:status,0) }
+      process.kill { |signal,pid| tp.kill(signal, pid) }
+    end
+  end
+
   # - - - - - - - - - - - - - - - - - - - - -
 
   class StdoutStderrThreaderStub
@@ -27,6 +78,8 @@ class DoubleRunRedAmberGreenTest < TestBase
     end
   end
 
+  # - - - - - - - - - - - - - - - - -
+
   class ThreadStub
     def initialize(name, value)
       @name = name
@@ -35,36 +88,6 @@ class DoubleRunRedAmberGreenTest < TestBase
     def value
       @value
     end
-    def join(_secs)
-      self
-    end
   end
-
-  # - - - - - - - - - - - - - - - - -
-
-  test 'd56', %w( red ) do
-    if ENV['CONTEXT'] === 'server'
-      mx_stderr = 'Test Count: 3, Passed: 2, Failed: 1, Warnings: 0, Inconclusive: 0, Skipped: 0'
-      stdout_tgz = TGZ.of({'stderr' => mx_stderr})
-      stderr = ''
-      @context = Context.new(
-        logger:StdoutLoggerSpy.new,
-        process:process=ProcessAdapterStub.new,
-        threader:StdoutStderrThreaderStub.new(stdout_tgz, stderr)
-      )
-      puller.add(image_name)
-      tp = ProcessAdapter.new
-      process.spawn { |_cmd,opts| tp.spawn('sleep 10', opts) }
-      process.detach { |pid| tp.detach(pid); ThreadStub.new(:status,0) }
-      process.kill { |signal,pid| tp.kill(signal, pid) }
-    end
-
-    run_cyber_dojo_sh
-
-    assert red?, run_result
-  end
-
-  # - - - - - - - - - - - - - - - - -
-
 
 end
