9e4e02a8e65455851f4ccc969852f656ef2309b6
Jon Jagger <jon@jaggersoft.com>
2020-12-06 14:01:40 +0000

    [ci skip] Added comment.

--- test/coverage.rb
+++ test/coverage.rb
@@ -12,6 +12,6 @@ SimpleCov.start do
   #add_group('debug') { |src| puts src.filename; false }
   code_tab = ENV['COVERAGE_CODE_TAB_NAME']
   test_tab = ENV['COVERAGE_TEST_TAB_NAME']
-  add_group(code_tab) { |src| src.filename =~ %r"^/runner/" }
+  add_group(code_tab) { |src| src.filename =~ %r"^/runner/" } # Dockefile WORKDIR
   add_group(test_tab) { |src| src.filename =~ %r"^/test/.*_test\.rb$" }
 end
