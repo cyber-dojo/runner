681a9e57e2db105eb11cf28bc01bc181ce56d5f4
Jon Jagger <jon@jaggersoft.com>
2020-12-08 09:28:02 +0000

    Only pull images needed for testing if not already present.

--- scripts/pull_dependent_images.sh
+++ scripts/pull_dependent_images.sh
@@ -5,11 +5,15 @@ pull_dependent_images()
 {
   echo
   echo Pulling images used in server-side tests
-  # alpine:latest is used for tests showing bash must be in the image_name
-  echo alpine:latest
-  docker pull alpine:latest
 
-  local -r display_names="$(
+  local -r IMAGE_NAMES=$(docker image ls --format '{{.Repository}}:{{.Tag}}')
+
+  if ! echo "${IMAGE_NAMES}" | grep alpine:latest ; then
+    # alpine:latest is used for tests showing bash must be in the image_name
+    docker pull alpine:latest
+  fi
+
+  local -r DISPLAY_NAMES="$(
     docker run \
       --entrypoint='' \
       --rm \
@@ -17,15 +21,16 @@ pull_dependent_images()
         ${CYBER_DOJO_RUNNER_IMAGE}:${CYBER_DOJO_RUNNER_TAG} \
           ruby /test/dependent_display_names.rb)"
 
-  local -r json_data=$(docker run --rm \
+  local -r JSON_DATA=$(docker run --rm \
     ${CYBER_DOJO_LANGUAGES_START_POINTS_IMAGE}:${CYBER_DOJO_LANGUAGES_START_POINTS_TAG} \
     bash -c 'ruby /app/repos/inspect.rb')
 
-  echo "${display_names}" \
+  echo "${DISPLAY_NAMES}" \
     | while read display_name
       do
-        local image_name=$(echo "${json_data}" | jq --raw-output ".[\"${display_name}\"].image_name")
-        echo "${image_name}"
-        docker pull "${image_name}"
+        local image_name=$(echo "${JSON_DATA}" | jq --raw-output ".[\"${display_name}\"].image_name")
+        if ! echo "${IMAGE_NAMES}" | grep "${image_name}" ; then
+          docker pull "${image_name}"
+        fi
       done
 }
