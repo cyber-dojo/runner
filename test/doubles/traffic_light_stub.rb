ac36c6e1b2991139c158b51159db2a0b487cfc18
Jon Jagger <jon@jaggersoft.com>
2020-12-09 08:07:09 +0000

    Removed dead code.

--- test/doubles/traffic_light_stub.rb
+++ test/doubles/traffic_light_stub.rb
@@ -6,10 +6,6 @@ class TrafficLightStub
     @stubbed = colour
   end
 
-  def colour(_image_name, _stdout, _stderr, _status)
-    @stubbed
-  end
-
   @@red   = TrafficLightStub.new('red')
   @@amber = TrafficLightStub.new('amber')
   @@green = TrafficLightStub.new('green')
