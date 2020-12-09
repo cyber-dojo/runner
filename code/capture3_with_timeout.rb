393c497db63b619688ff50586f01de57bfdf1e32
Jon Jagger <jon@jaggersoft.com>
2020-12-09 19:57:55 +0000

    Refactoring; simplify capture3_with_timeout.

--- code/capture3_with_timeout.rb
+++ code/capture3_with_timeout.rb
@@ -5,30 +5,23 @@ module Capture3WithTimeout
 
   def capture3_with_timeout(context, command, max_seconds, tgz_in)
     # Based on https://gist.github.com/pasela/9392115
-    spawn_opts = {
-      pgroup:true # make a new process group
-    }
+    piper    = context.piper
+    process  = context.process
+    threader = context.threader
 
-    piper = context.piper
     stdin_pipe  = piper.io
-    stdout_pipe = piper.io
-    stderr_pipe = piper.io
+    stdin_pipe.out.binmode
     stdin_pipe.out.sync = true
 
-    stdin_pipe.out.binmode
+    stdout_pipe = piper.io
     stdout_pipe.in.binmode
-    stderr_pipe.in.binmode
 
-    spawn_opts[:in]  = stdin_pipe.in
-    spawn_opts[:out] = stdout_pipe.out
-    spawn_opts[:err] = stderr_pipe.out
+    stderr_pipe = piper.io
+    stderr_pipe.in.binmode
 
     stdout_reader_thr = nil
     stderr_reader_thr = nil
     wait_thr = nil
-
-    process  = context.process
-    threader = context.threader
 
     result = {
       timed_out:false,
@@ -41,7 +34,12 @@ module Capture3WithTimeout
 
     begin
       Timeout.timeout(max_seconds) do
-        pid = process.spawn(command, spawn_opts)
+        pid = process.spawn(command, {
+          pgroup:true, # make a new process group
+              in: stdin_pipe.in,
+             out: stdout_pipe.out,
+             err: stderr_pipe.out
+        })
         wait_thr = process.detach(pid)
         stdin_pipe.in.close
         stdout_pipe.out.close
