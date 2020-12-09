17de339eda4ff8ecddb01baa83d443fcf7ed6459
Jon Jagger <jon@jaggersoft.com>
2020-12-09 20:04:08 +0000

    Refactoring; simplify capture3_with_timeout.

--- code/capture3_with_timeout.rb
+++ code/capture3_with_timeout.rb
@@ -13,15 +13,15 @@ module Capture3WithTimeout
     stdin_pipe.out.binmode
     stdin_pipe.out.sync = true
 
-    stdout_pipe = piper.io
+    stdout_pipe = piper.io # multiplexed cyber-dojo.sh's stdout/stderr/status)
     stdout_pipe.in.binmode
 
     stderr_pipe = piper.io
     stderr_pipe.in.binmode
 
-    stdout_reader_thr = nil
-    stderr_reader_thr = nil
-    wait_thr = nil
+    stdout_reader_thread = nil
+    stderr_reader_thread = nil
+    wait_thread = nil
 
     result = {
       timed_out:false,
@@ -40,29 +40,29 @@ module Capture3WithTimeout
              out: stdout_pipe.out,
              err: stderr_pipe.out
         })
-        wait_thr = process.detach(pid)
+        wait_thread = process.detach(pid)
         stdin_pipe.in.close
         stdout_pipe.out.close
         stderr_pipe.out.close
-        stdout_reader_thr = threader.thread { stdout_pipe.in.read }
-        stderr_reader_thr = threader.thread { stderr_pipe.in.read }
+        stdout_reader_thread = threader.thread { stdout_pipe.in.read }
+        stderr_reader_thread = threader.thread { stderr_pipe.in.read }
         stdin_pipe.out.write(tgz_in)
         stdin_pipe.out.close
-        result[:status] = wait_thr.value
+        result[:status] = wait_thread.value
       end
     rescue Timeout::Error
       result[:timed_out] = true
       unless pid.nil?
         process.kill(:TERM, -pid)
-        unless wait_thr.join(1)
+        unless wait_thread.join(1)
           process.kill(:KILL, -pid)
         end
       end
       yield
     ensure
-      result[:status] = wait_thr.value if wait_thr
-      result[:stdout] = stdout_reader_thr.value if stdout_reader_thr
-      result[:stderr] = stderr_reader_thr.value if stderr_reader_thr
+      result[:status] = wait_thread.value if wait_thread
+      result[:stdout] = stdout_reader_thread.value if stdout_reader_thread
+      result[:stderr] = stderr_reader_thread.value if stderr_reader_thread
       stdout_pipe.out.close unless stdout_pipe.out.closed?
       stderr_pipe.out.close unless stderr_pipe.out.closed?
     end
