e4d04a1de92a1ffb5197cc651055e9337df6c782
Jon Jagger <jon@jaggersoft.com>
2020-12-10 12:10:51 +0000

    Refactoring; moved code into Capture3WithTimout class.

--- code/capture3_with_timeout.rb
+++ code/capture3_with_timeout.rb
@@ -2,85 +2,84 @@
 require 'timeout'
 
 class Capture3WithTimeout
-  def initialize(context)
-    @context = context
-  end
-  def run(command, max_seconds, tgz_in, &block)
-    capture3_with_timeout(@context, command, max_seconds, tgz_in, &block)
-  end
-end
-
-def capture3_with_timeout(context, command, max_seconds, tgz_in)
   # Based on https://gist.github.com/pasela/9392115
-  result = { timed_out:false }
 
-  piper    = context.piper
-  process  = context.process
-  threader = context.threader
+  def initialize(context)
+    @piper    = context.piper
+    @process  = context.process
+    @threader = context.threader
+  end
 
-  stdin_pipe = piper.io
-  stdin_pipe.out.binmode
-  stdin_pipe.out.sync = true
+  def run(command, max_seconds, tgz_in)
+    result = { timed_out:false }
 
-  stdout_pipe = piper.io # multiplexed cyber-dojo.sh's stdout/stderr/status
-  stdout_pipe.in.binmode
+    stdin_pipe = @piper.io
+    stdin_pipe.out.binmode
+    stdin_pipe.out.sync = true
 
-  stderr_pipe = piper.io
-  stderr_pipe.in.binmode
+    stdout_pipe = @piper.io # multiplexed cyber-dojo.sh's stdout/stderr/status
+    stdout_pipe.in.binmode
 
-  stdout_reader_thread = ThreadNilValue.new
-  stderr_reader_thread = ThreadNilValue.new
-  wait_thread = ThreadNilValue.new
+    stderr_pipe = @piper.io
+    stderr_pipe.in.binmode
 
-  pid = nil
+    stdout_reader_thread = ThreadNilValue.new
+    stderr_reader_thread = ThreadNilValue.new
+    wait_thread = ThreadNilValue.new
 
-  begin
-    Timeout.timeout(max_seconds) do
-      pid = process.spawn(command, {
-        pgroup:true, # make a new process group
-            in: stdin_pipe.in,
-           out: stdout_pipe.out,
-           err: stderr_pipe.out
-      })
-      wait_thread = process.detach(pid) # prevent zombie child processes
-      stdin_pipe.in.close
-      stdout_pipe.out.close
-      stderr_pipe.out.close
-      stdout_reader_thread = threader.thread { stdout_pipe.in.read }
-      stderr_reader_thread = threader.thread { stderr_pipe.in.read }
-      stdin_pipe.out.write(tgz_in)
-      stdin_pipe.out.close
-      result[:status] = wait_thread.value
-    end
-  rescue Timeout::Error
-    result[:timed_out] = true
-    unless pid.nil?
-      process.kill(:TERM, -pid)
-      if wait_thread.join(1).nil?
-        # process.kill(:TERM,-pid) did not return after 1 second
-        process.kill(:KILL, -pid)
+    pid = nil
+
+    begin
+      Timeout.timeout(max_seconds) do
+        pid = @process.spawn(command, {
+          pgroup:true, # make a new process group
+              in: stdin_pipe.in,
+             out: stdout_pipe.out,
+             err: stderr_pipe.out
+        })
+        wait_thread = @process.detach(pid) # prevent zombie child processes
+        stdin_pipe.in.close
+        stdout_pipe.out.close
+        stderr_pipe.out.close
+        stdout_reader_thread = @threader.thread { stdout_pipe.in.read }
+        stderr_reader_thread = @threader.thread { stderr_pipe.in.read }
+        stdin_pipe.out.write(tgz_in)
+        stdin_pipe.out.close
+        result[:status] = wait_thread.value
       end
+    rescue Timeout::Error
+      result[:timed_out] = true
+      unless pid.nil?
+        @process.kill(:TERM, -pid)
+        if wait_thread.join(1).nil?
+          # process.kill(:TERM,-pid) did not return after 1 second
+          @process.kill(:KILL, -pid)
+        end
+      end
+      yield
+    ensure
+      result[:status] = wait_thread.value
+      result[:stdout] = stdout_reader_thread.value
+      result[:stderr] = stderr_reader_thread.value
+      safe_close(stdout_pipe.out)
+      safe_close(stderr_pipe.out)
     end
-    yield
-  ensure
-    result[:status] = wait_thread.value
-    result[:stdout] = stdout_reader_thread.value
-    result[:stderr] = stderr_reader_thread.value
-    safe_close(stdout_pipe.out)
-    safe_close(stderr_pipe.out)
+
+    result
   end
 
-  result
-end
+  private
 
-def safe_close(out)
-  unless out.closed?
-    out.close
+  def safe_close(out)
+    unless out.closed?
+      out.close
+    end
   end
-end
 
-class ThreadNilValue
-  def value
-    nil
+  class ThreadNilValue
+    def value
+      nil
+    end
   end
+
 end
