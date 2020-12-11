1f22c2c3d168fd8250b6f80fed80c6060b828091
Jon Jagger <jon@jaggersoft.com>
2020-12-11 09:42:51 +0000

    Refactoring; collapse test duplication.

--- test/server/docker_stop_test.rb
+++ test/server/docker_stop_test.rb
@@ -15,22 +15,10 @@ class DockerStopTest < TestBase
   then docker stop is called
   and a docker-stop call failure is logged
   ) do
-    set_context(
-        logger:logger=StdoutLoggerSpy.new,
-         piper:PiperStub.new(''),
-       process:process=ProcessSpawnerStub.new,
-       sheller:sheller=BashShellerStub.new,
-      threader:ThreaderStub.new,
-        random:RandomHex8Stub.new(hex8_stub='a1b2c3d4')
-    )
+    setup_stubs
 
-    process.spawn { |_cmd,_opts| 42 } # pid
-    process.detach { |_pid| WaitThreadTimedOutStub.new(57, false) } # status,joined
-    process.kill { |_signal,_pid| nil }
-
-    expected_docker_stop_command = "docker stop --time 1 cyber_dojo_runner_#{id58}_#{hex8_stub}"
     docker_stop_called = false
-    sheller.capture(expected_docker_stop_command) { |_command|
+    @sheller.capture(expected_docker_stop_command) { |_command|
       docker_stop_called = true
       ['stdout-stub','Docker-daemon-error',125]
     }
@@ -39,24 +27,24 @@ class DockerStopTest < TestBase
     run_cyber_dojo_sh(max_seconds:3)
     assert docker_stop_called
 
-    lines = logger.logged.lines
+    lines = @logger.logged.lines
     assert_equal 2, lines.size
 
-    assert_equal({
-      "id" => id58,
-      "image_name" => image_name,
-      "command" => expected_docker_stop_command,
-      "stdout" => 'stdout-stub',
-      "stderr" => 'Docker-daemon-error',
-      "status" => 125
-    }, JSON.parse(lines[0]))
+    assert_json_line(lines[0], {
+      id:id58,
+      image_name:image_name,
+      command:expected_docker_stop_command,
+      stdout:'stdout-stub',
+      stderr:'Docker-daemon-error',
+      status:125
+    })
 
-    assert_equal({
-      "id" => id58,
-      "image_name" => image_name,
-      "message" => "timed_out",
-      "result" => ""
-    }, JSON.parse(lines[1]))
+    assert_json_line(lines[1], {
+      id:id58,
+      image_name:image_name,
+      message:'timed_out',
+      result:''
+    })
   end
 
   # - - - - - - - - - - - - - - - - - - - - -
@@ -66,42 +54,60 @@ class DockerStopTest < TestBase
   then docker stop is called
   and a docker-stop call success leaves the log empty
   ) do
-    set_context(
-        logger:logger=StdoutLoggerSpy.new,
-         piper:PiperStub.new(''),
-       process:process=ProcessSpawnerStub.new,
-       sheller:sheller=BashShellerStub.new,
-      threader:ThreaderStub.new,
-        random:RandomHex8Stub.new(hex8_stub='a1b2c3d4')
-    )
+    setup_stubs
 
-    process.spawn { |_cmd,_opts| 42 } # pid
-    process.detach { |_pid| WaitThreadTimedOutStub.new(57, false) } # status,joined
-    process.kill { |_signal,_pid| nil }
-
-    expected_docker_stop_command = "docker stop --time 1 cyber_dojo_runner_#{id58}_#{hex8_stub}"
     docker_stop_called = false
-    sheller.capture(expected_docker_stop_command) { |_command|
+    @sheller.capture(expected_docker_stop_command) { |_command|
       docker_stop_called = true
-      ['','',0]
+      ['s1','s2',0]
     }
 
     puller.add(image_name)
     run_cyber_dojo_sh(max_seconds:3)
     assert docker_stop_called
 
-    lines = logger.logged.lines
+    lines = @logger.logged.lines
     assert_equal 1, lines.size
 
-    assert_equal({
-      "id" => id58,
-      "image_name" => image_name,
-      "message" => "timed_out",
-      "result" => ""
-    }, JSON.parse(lines[0]))
+    assert_json_line(lines[0], {
+      id:id58,
+      image_name:image_name,
+      message:'timed_out',
+      result:''
+    })
   end
 
   private
+
+  def setup_stubs
+    set_context(
+        logger:@logger=StdoutLoggerSpy.new,
+         piper:PiperStub.new(''),
+       process:process=ProcessSpawnerStub.new,
+       sheller:@sheller=BashShellerStub.new,
+      threader:ThreaderStub.new,
+        random:RandomHex8Stub.new(hex8_stub)
+    )
+    process.spawn { |_cmd,_opts| 42 } # pid
+    process.detach { |_pid| WaitThreadTimedOutStub.new(57, false) } # status,joined
+    process.kill { |_signal,_pid| nil }
+  end
+
+  def expected_docker_stop_command
+    "docker stop --time 1 cyber_dojo_runner_#{id58}_#{hex8_stub}"
+  end
+
+  def hex8_stub
+    'a1b2c3d4'
+  end
+
+  # - - - - - - - - - - - - - - - - -
+
+  def assert_json_line(line, expected)
+    assert_equal(expected, JSON.parse(line,symbolize_names:true))
+  end
+
+  # - - - - - - - - - - - - - - - - -
 
   class ThreaderStub
     def thread(name)
