4664da75c56685e11d80aef99ba9a6fd086cd37f
Jon Jagger <jon@jaggersoft.com>
2021-01-17 13:01:34 +0000

    Add comment explaining reason deleted files are no longer detected.

--- code/files_delta.rb
+++ code/files_delta.rb
@@ -20,4 +20,30 @@ module FilesDelta
     [ new, deleted, changed ]
   end
 
+=begin
+  The names of deleted files are NOT returned to the caller.
+
+  The intended illusion in cyber-dojo is that the test run
+  is happening in the browser. Thus the only way you should be
+  able to delete a file is directly from the browser by clicking
+  the [-] button.
+
+  Deleted files used to be detected in files_delta() but it caused problems:
+
+  1) It caused unwanted diffs between test runs.
+
+     For example, if you generate coverage files, but only on a green test,
+     then a green test, followed by a red test results in all the coverage files
+     unhelpfully appearing as deleted files in the diff view.
+
+  2) It resulted in files being deleted for no apparant reason!
+
+     Suppose, in the browser, you accidentally type a rogue character into a file.
+     - The file is saved into the container.
+     - The os thinks it is a binary file.
+     - The text-file harvester does NOT see it.
+     - Its name is returned as a deleted file.
+     - The browser deletes it!
+=end
+
 end
