caf14412dca67f28dd408372335d65483fa8a635
Jon Jagger <jon@jaggersoft.com>
2021-08-07 12:55:13 +0100

    Tidy merkely scripts

--- .circleci/merkely_log_deployment.sh
+++ .circleci/merkely_log_deployment.sh
@@ -6,6 +6,9 @@ MERKELY_CHANGE=merkely/change:latest
 MERKELY_OWNER=cyber-dojo
 MERKELY_PIPELINE=runner
 
+readonly ENVIRONMENT="${1}"
+readonly HOSTNAME="${2}"
+
 # - - - - - - - - - - - - - - - - - - -
 merkely_fingerprint()
 {
@@ -15,25 +18,10 @@ merkely_fingerprint()
 # - - - - - - - - - - - - - - - - - - -
 merkely_log_deployment()
 {
-  local -r environment="${1}"
-
   VERSIONER_URL=https://raw.githubusercontent.com/cyber-dojo/versioner/master
   export $(curl "${VERSIONER_URL}/app/.env")
   export CYBER_DOJO_RUNNER_TAG="${CIRCLE_SHA1:0:7}"
   docker pull ${CYBER_DOJO_RUNNER_IMAGE}:${CYBER_DOJO_RUNNER_TAG}
-
-  docker run \
-    --env MERKELY_COMMAND=log_deployment \
-    --env MERKELY_OWNER=${MERKELY_OWNER} \
-    --env MERKELY_PIPELINE=${MERKELY_PIPELINE} \
-    --env MERKELY_FINGERPRINT=$(merkely_fingerprint) \
-    --env MERKELY_DESCRIPTION="Deployed to ${environment} in circleci pipeline" \
-    --env MERKELY_ENVIRONMENT="${environment}" \
-    --env MERKELY_CI_BUILD_URL=${CIRCLE_BUILD_URL} \
-    --env MERKELY_API_TOKEN=${MERKELY_API_TOKEN} \
-    --rm \
-    --volume /var/run/docker.sock:/var/run/docker.sock \
-    	${MERKELY_CHANGE}
 
 	docker run \
       --env MERKELY_COMMAND=log_deployment \
@@ -41,13 +29,13 @@ merkely_log_deployment()
       --env MERKELY_PIPELINE=${MERKELY_PIPELINE} \
       --env MERKELY_FINGERPRINT=$(merkely_fingerprint) \
       --env MERKELY_DESCRIPTION="Deployed to ${environment} in circleci pipeline" \
-      --env MERKELY_ENVIRONMENT="${environment}" \
+      --env MERKELY_ENVIRONMENT="${ENVIRONMENT}" \
       --env MERKELY_CI_BUILD_URL=${CIRCLE_BUILD_URL} \
       --env MERKELY_API_TOKEN=${MERKELY_API_TOKEN} \
-      --env MERKELY_HOST=https://staging.app.merkely.com \
+      --env MERKELY_HOST="${HOSTNAME}" \
       --rm \
       --volume /var/run/docker.sock:/var/run/docker.sock \
-    	${MERKELY_CHANGE}
+    	    ${MERKELY_CHANGE}
 }
 
-merkely_log_deployment "${1}"
+merkely_log_deployment
--- scripts/merkely.sh
+++ scripts/merkely.sh
@@ -13,53 +13,25 @@ merkely_fingerprint()
 }
 
 # - - - - - - - - - - - - - - - - - - -
-on_ci_merkely_declare_pipeline()
+merkely_declare_pipeline()
 {
-  if ! on_ci ; then
-    return
-  fi
-  
-  docker run \
-	--env MERKELY_COMMAND=declare_pipeline \
-    --env MERKELY_OWNER=${MERKELY_OWNER} \
-    --env MERKELY_PIPELINE=${MERKELY_PIPELINE} \
-	--env MERKELY_API_TOKEN=${MERKELY_API_TOKEN} \
-	--rm \
-	--volume ${ROOT_DIR}/Merkelypipe.json:/data/Merkelypipe.json \
-		${MERKELY_CHANGE}
+  local -r hostname="${1}"
 
   docker run \
-	--env MERKELY_COMMAND=declare_pipeline \
+  	--env MERKELY_COMMAND=declare_pipeline \
     --env MERKELY_OWNER=${MERKELY_OWNER} \
     --env MERKELY_PIPELINE=${MERKELY_PIPELINE} \
-	--env MERKELY_API_TOKEN=${MERKELY_API_TOKEN} \
-    --env MERKELY_HOST=https://staging.app.merkely.com \
-	--rm \
-	--volume ${ROOT_DIR}/Merkelypipe.json:/data/Merkelypipe.json \
-		${MERKELY_CHANGE}
+	  --env MERKELY_API_TOKEN=${MERKELY_API_TOKEN} \
+    --env MERKELY_HOST="${hostname}" \
+	  --rm \
+	  --volume ${ROOT_DIR}/Merkelypipe.json:/data/Merkelypipe.json \
+		  ${MERKELY_CHANGE}
 }
 
 # - - - - - - - - - - - - - - - - - - -
-on_ci_merkely_log_artifact()
+merkely_log_artifact()
 {
-  if ! on_ci ; then
-    return
-  fi
- 
-  docker run \
-    --env MERKELY_COMMAND=log_artifact \
-    --env MERKELY_OWNER=${MERKELY_OWNER} \
-    --env MERKELY_PIPELINE=${MERKELY_PIPELINE} \
-    --env MERKELY_FINGERPRINT=$(merkely_fingerprint) \
-    --env MERKELY_IS_COMPLIANT=TRUE \
-    --env MERKELY_ARTIFACT_GIT_COMMIT=${CYBER_DOJO_RUNNER_SHA} \
-    --env MERKELY_ARTIFACT_GIT_URL=https://github.com/${MERKELY_OWNER}/${MERKELY_PIPELINE}/commit/${CYBER_DOJO_RUNNER_SHA} \
-    --env MERKELY_CI_BUILD_NUMBER=${CIRCLE_BUILD_NUM} \
-    --env MERKELY_CI_BUILD_URL=${CIRCLE_BUILD_URL} \
-    --env MERKELY_API_TOKEN=${MERKELY_API_TOKEN} \
-    --rm \
-    --volume /var/run/docker.sock:/var/run/docker.sock \
-    ${MERKELY_CHANGE}
+  local -r hostname="${1}"
 
   docker run \
     --env MERKELY_COMMAND=log_artifact \
@@ -72,36 +44,16 @@ on_ci_merkely_log_artifact()
     --env MERKELY_CI_BUILD_NUMBER=${CIRCLE_BUILD_NUM} \
     --env MERKELY_CI_BUILD_URL=${CIRCLE_BUILD_URL} \
     --env MERKELY_API_TOKEN=${MERKELY_API_TOKEN} \
-    --env MERKELY_HOST=https://staging.app.merkely.com \
+    --env MERKELY_HOST="${hostname}" \
     --rm \
     --volume /var/run/docker.sock:/var/run/docker.sock \
-    ${MERKELY_CHANGE}
+      ${MERKELY_CHANGE}
 }
 
 # - - - - - - - - - - - - - - - - - - -
-on_ci_merkely_log_evidence()
+merkely_log_evidence()
 {
-  if ! on_ci ; then
-    return
-  fi
-
-  write_evidence_json
-  
-  docker run \
-    --env MERKELY_COMMAND=log_evidence \
-    --env MERKELY_OWNER=${MERKELY_OWNER} \
-    --env MERKELY_PIPELINE=${MERKELY_PIPELINE} \
-    --env MERKELY_FINGERPRINT=$(merkely_fingerprint) \
-    --env MERKELY_EVIDENCE_TYPE=branch-coverage \
-    --env MERKELY_IS_COMPLIANT=TRUE \
-    --env MERKELY_DESCRIPTION="server & client branch-coverage reports" \
-    --env MERKELY_USER_DATA="$(evidence_json_path)" \
-    --env MERKELY_CI_BUILD_URL=${CIRCLE_BUILD_URL} \
-    --env MERKELY_API_TOKEN=${MERKELY_API_TOKEN} \
-    --rm \
-    --volume "$(evidence_json_path):$(evidence_json_path)" \
-    --volume /var/run/docker.sock:/var/run/docker.sock \
-    ${MERKELY_CHANGE}
+  local -r hostname="${1}"
 
   docker run \
     --env MERKELY_COMMAND=log_evidence \
@@ -114,11 +66,11 @@ on_ci_merkely_log_evidence()
     --env MERKELY_USER_DATA="$(evidence_json_path)" \
     --env MERKELY_CI_BUILD_URL=${CIRCLE_BUILD_URL} \
     --env MERKELY_API_TOKEN=${MERKELY_API_TOKEN} \
-    --env MERKELY_HOST=https://staging.app.merkely.com \
+    --env MERKELY_HOST="${hostname}" \
     --rm \
     --volume "$(evidence_json_path):$(evidence_json_path)" \
     --volume /var/run/docker.sock:/var/run/docker.sock \
-    ${MERKELY_CHANGE}
+      ${MERKELY_CHANGE}
 }
 
 # - - - - - - - - - - - - - - - - - - -
@@ -142,3 +94,39 @@ on_ci()
 {
   [ -n "${CIRCLECI:-}" ]
 }
+
+# - - - - - - - - - - - - - - - - - - -
+on_ci_merkely_declare_pipeline()
+{
+  if ! on_ci ; then
+    return
+  fi
+
+  merkely_declare_pipeline https://staging.app.merkely.com
+  merkely_declare_pipeline https://app.merkely.com
+}
+
+# - - - - - - - - - - - - - - - - - - -
+on_ci_merkely_log_artifact()
+{
+  if ! on_ci ; then
+    return
+  fi
+
+  merkely_log_artifact https://staging.app.merkely.com
+  merkely_log_artifact https://app.merkely.com
+}
+
+# - - - - - - - - - - - - - - - - - - -
+on_ci_merkely_log_evidence()
+{
+  if ! on_ci ; then
+    return
+  fi
+
+  write_evidence_json
+
+  merkely_log_evidence https://staging.app.merkely.com
+  merkely_log_evidence https://app.merkely.com
+}
+
