d2bc1c284437042e758886768fc7c071e37a3081
zhelezovartem <Zhelezovartem@gmail.com>
2022-08-02 18:19:34 +0300

    add terraform specs for the service deployment

---
+++ deployment/terraform/variables.tf
@@ -0,0 +1,52 @@
+variable "service_name" {
+  type = string
+  default = "runner"
+}
+
+variable "env" {
+  type = string
+}
+
+variable "app_port" {
+  type = number
+  default = 4597
+}
+
+variable "cpu_limit" {
+  type = number
+  default = 20
+}
+
+variable "mem_limit" {
+  type = number
+  default = 768
+}
+
+variable "mem_reservation" {
+  type = number
+  default = 128
+}
+
+variable "TAGGED_IMAGE" {
+  type = string
+}
+
+# App variables
+variable "app_env_vars" {
+  type = map(any)
+  default = {
+    CYBER_DOJO_USE_CONTAINERD = "true"
+    CYBER_DOJO_PROMETHEUS     = "false"
+    CYBER_DOJO_RUNNER_PORT    = "4597"
+  }
+}
+
+variable "ecr_replication_targets" {
+  type    = list(map(string))
+  default = []
+}
+
+variable "ecr_replication_origin" {
+  type    = string
+  default = ""
+}
