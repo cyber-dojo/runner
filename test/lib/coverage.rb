5ea5be455751ab12d7e169b8399708496c225082
JonJagger <jon@kosli.com>
2024-10-29 10:48:52 +0000

    Fix rubocop warnings

--- test/lib/coverage.rb
+++ test/lib/coverage.rb
@@ -11,21 +11,20 @@ end
 SimpleCov.start do
   enable_coverage :branch
   filters.clear
-  coverage_dir("#{ENV.fetch('COVERAGE_ROOT', nil)}")
+  coverage_dir(ENV.fetch('COVERAGE_ROOT', nil).to_s)
   nocov_token(runner_nocov_token)
 
   code_tab = ENV.fetch('COVERAGE_CODE_TAB_NAME', nil)
   test_tab = ENV.fetch('COVERAGE_TEST_TAB_NAME', nil)
 
-  #add_group('debug') { |src| puts src.filename; false }
+  # add_group('debug') { |src| puts src.filename; false }
 
-  add_group(test_tab) { |src|
-    src.filename.start_with?("/runner/test/#{CONTEXT}/") ||
-    src.filename.start_with?("/runner/test/dual/")
-  }
-  add_group(code_tab) { |src|
-    !src.filename.start_with?("/runner/test/")
-  }
+  add_group(test_tab) do |src|
+    src.filename.start_with?("/runner/test/#{CONTEXT}/") || src.filename.start_with?('/runner/test/dual/')
+  end
+  add_group(code_tab) do |src|
+    !src.filename.start_with?('/runner/test/')
+  end
 end
 
 formatters = [
