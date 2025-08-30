83df3a4d1321739421f9724c29cb7949e4e4838f
JonJagger <jon@kosli.com>
2025-08-30 10:56:49 +0100

    Update test/data/display_names.rb with new gcc/clang names

--- test/data/display_names.rb
+++ test/data/display_names.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 module DisplayNames
-  ALPINE = 'C (gcc), assert'
+  ALPINE = 'C (gcc 14.3.0), assert'
   DEBIAN = 'Perl, Test::Simple'
-  UBUNTU = 'C (clang), assert'
+  UBUNTU = 'C (clang 14.0.0), assert'
 end
