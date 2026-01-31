061fc4407fe4a824a2c426b40cd3286cca995a09
JonJagger <jon@kosli.com>
2026-01-31 07:45:24 +0000

    Move echo_env_vars.sh into its own file

--- bin/demo.sh
+++ bin/demo.sh
@@ -2,12 +2,13 @@
 set -Ee
 
 export ROOT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")/.." && pwd)"
+source "${ROOT_DIR}/bin/lib.sh"
+source "${ROOT_DIR}/bin/echo_env_vars.sh"
+# shellcheck disable=SC2046
+export $(echo_env_vars)
 
 readonly DEMO_FILENAME="/tmp/runner_demo.html"
 
-source "${ROOT_DIR}/bin/lib.sh"
-# shellcheck disable=SC2046
-export $(echo_env_vars)
 docker compose --progress=plain up --wait --wait-timeout=10 client
 docker exec -it test_runner_client ruby /runner/demo.rb > "${DEMO_FILENAME}"
 open "file://${DEMO_FILENAME}"
